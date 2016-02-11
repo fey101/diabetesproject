2c1f54ecd631dcdbe6a91f8f343420464058979d
faith kangai <faith.kangai@savannahinformatics.com>
2016-02-11 11:19:50 +0300

    modify recipe app

--- diabetescheck/recipe/models.py
+++ diabetescheck/recipe/models.py
@@ -1,11 +1,15 @@
 from django.db import models
-from django.conf import settings
+from journal.models import Person
 
-OPTIONS = (
-    ('carbohydrates', 'Carbohydrates'),
-    ('proteins', 'Proteins'),
+
+CATEGORY_OPTIONS = (
+    ('carbohydrates', 'Carbohydrate'),
+    ('proteins', 'Protein'),
     ('alcohol', 'Alcohol'),
-    ('fats', 'Fats and Oils')
+    ('fats', 'Fats and Oils'),
+    ('vitamins', 'Vitamin'),
+    ('cholestrols', 'Cholestrol'),
+    ('minerals', 'Mineral')
 )
 
 
@@ -18,7 +22,9 @@ CALORIC_CONSTANTS = {
 
 
 class Recipe(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    """Available recipes."""
+
+    user = models.ForeignKey(Person)
     name = models.CharField(max_length=255)
     ingredients = models.ManyToManyField(
         'FoodItem', related_name='FoodItem_recipes')
@@ -27,44 +33,56 @@ class Recipe(models.Model):
     serving = models.IntegerField()
 
     def __str__(self):
+        """String representation of Recipe."""
         return self.name
 
 
 class FoodItem(models.Model):
-    users = models.ManyToManyField(settings.AUTH_USER_MODEL)
+    """A distinguishable, simplest food item/ingredient."""
+
     name = models.CharField(max_length=255)
-    category = models.ManyToManyField(
-        'FoodCategory', through='FoodItemCategory')
+    nutritional_value = models.ManyToManyField(
+        'FoodCategory', through='NutritionalValue')
+    # quantity of FoodItem consumed in grams
+    amount = models.FloatField()
 
     def __str__(self):
+        """Return a string representation of the FoodItem."""
         return self.name
-    # carbohydrates = models.FloatField()
-    # vitamins = models.FloatField()
-    # proteins = models.FloatField()
-    # cholestrol = models.FloatField()
-    # oils = models.FloatField()
-    # fats = models.FloatField()
-    # alcohol = models.FloatField()
-    # sodium = models.FloatField()
 
-    # @property
-    # def calories(self):
-    #     from_proteins = self.proteins * CALORIC_CONSTANTS['proteins']
-    #     from_carbs = self.carbohydrates * CALORIC_CONSTANTS['carbohydrates']
-    #     from_fats = self.fats * CALORIC_CONSTANTS['fats']
-    #     from_alcohol = self.alcohol * CALORIC_CONSTANTS['alcohol']
-
-    #     return from_proteins + from_carbs + from_fats + from_alcohol
+    @property
+    def calories(self):
+        """Sum total of calories per consumed FoodItem."""
+        units_consumed = self.amount / 100
+        return self.fooditem_nutrients.calories_in_100g() * units_consumed
 
 
 class FoodCategory(models.Model):
-    category = models.CharField(max_length=25, choices=OPTIONS)
+    """Food categorised into proteins, carbohydrates, vitamins, e.t.c."""
+
+    category = models.CharField(max_length=25, choices=CATEGORY_OPTIONS)
     caloric_value_per_gram = models.FloatField()
 
     def __str__(self):
+        """Return category as string representation of FoodCategory class."""
         return self.category
 
 
-class FoodItemCategory(models.Model):
-    food_item = models.ForeignKey('FoodItem')
-    category = models.ForeignKey('FoodCategory')
+class NutritionalValue(models.Model):
+    """
+    Nutrients in a food_item.
+
+    Also a through table between FoodItem and FoodCategory models.
+    This model has pre-defined constants for example in 100g of milk there
+    is 9g of protein and 12g of carbohydrates.
+    """
+
+    food_item = models.ForeignKey(
+        'FoodItem', related_name="fooditem_nutrients")
+    category = models.ForeignKey(
+        'FoodCategory', related_name="category_nutrients")
+    quantity_per_100g = models.FloatField()
+
+    def calories_in_100g(self):
+        """Return calories contributed by a given nutrient in a food_item."""
+        return self.category.caloric_value_per_gram * self.quantity_per_100g
